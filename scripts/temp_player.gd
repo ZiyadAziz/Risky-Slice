1163a9d35751be54d6debadb171749867ff44648
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2025-08-26 00:14:55 -0400

    added more basic test stuff

--- scripts/temp_player.gd
+++ scripts/temp_player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,15 +9,21 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("attack_singleplayer"): #Need to add a timer or something so that the player has to wait for the animation to end before attacking again 
+		print("attacking")
+	
+	if Input.is_action_just_pressed("parry_singleplayer"):
+		print("parry")
+		
+	if Input.is_action_just_pressed("feint_singleplayer"):
+		print("feint")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	#Moving left and right
+	var direction := Input.get_axis("block_and_move_backwards_singleplayer", "move_forward_singleplayer")
+	if direction > 0: #move forwards
+		velocity.x = direction * SPEED 
+	elif direction < 0: #move backwards, currently moving backwards makes you move slower, but also would likely need to add blocking feature here
+		velocity.x = (direction * SPEED) / 2
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
