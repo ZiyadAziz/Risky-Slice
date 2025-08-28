22e5545b7ff08eb4eb6a1874591893ed45c7cad7
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2025-08-28 00:19:13 -0400

    I forgot what I added

--- scripts/temp_enemy.gd
+++ scripts/temp_enemy.gd
@@ -3,15 +3,16 @@ extends CharacterBody2D
 var health := 3
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
-	print("hit")
-	health -= 1
-	print(GameManager.p1_score)
-	if health <= 0:
-		print("dead")
-		GameManager.p1_score += 1
+	if area.is_in_group("Sword"):
+		print("hit")
+		health -= 1
 		print(GameManager.p1_score)
-		GameManager.reset_round()
-		print(GameManager.p1_score)
+		if health <= 0:
+			print("dead")
+			GameManager.p1_score += 1
+			print(GameManager.p1_score)
+			GameManager.reset_round()
+			print(GameManager.p1_score)
 		
 
 func _on_parry_area_area_entered(area: Area2D) -> void:
--- scripts/temp_player.gd
+++ scripts/temp_player.gd
@@ -14,25 +14,34 @@ var dodge_direction := 0
 var last_tap_time_left := -1.0
 var last_tap_time_right := -1.0
 
-func _physics_process(delta: float) -> void:
-	# Gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+#Attacking Code (will probs have to do something similar with parrying and feint)
+var isAttacking := false #Not sure if I want them to be able to attack while moving, (I dont want the to attack while dodging) 
+var isFeinting := false
+var isParrying := false
+var isBlocking := false
 
+func _physics_process(delta: float) -> void:
 	# Attack
 	if Input.is_action_just_pressed("attack_singleplayer"): #Need to add a timer or something so that the player has to wait for the animation to end before attacking again 
+		isAttacking = true #This will likely have to be moved to when the windup of the attack animation finishes 
 		$AttackArea/CollisionShape2D.disabled = false
 		await get_tree().create_timer(0.5).timeout #this probs shouldnt be the solution, I'd want to tie the timer to the attack animation when I make one 
 		$AttackArea/CollisionShape2D.disabled = true
+		isAttacking = false #This should be tied to the animation, but since I dont have one, this will do
 
 	#this should have a similar hitbox system as the attack, 
 	#but instead of looking for a player hitbox it would look for the attack hitbox, so im gonna have to do funny layer stuff i think
 	if Input.is_action_just_pressed("parry_singleplayer"):
+		$ParryArea/CollisionShape2D.disabled = false
+		await get_tree().create_timer(1).timeout
+		$ParryArea/CollisionShape2D.disabled = true
 		print("parry")
 
 	#This shouldnt have any hitbox, it should just play a feint animation to bait the enemy 
 	if Input.is_action_just_pressed("feint_singleplayer"):
 		print("feint")
+
+
 
 	# Handle dodge logic
 	if is_dodging:
@@ -42,6 +51,9 @@ func _physics_process(delta: float) -> void:
 			is_dodging = false
 	else:
 		var direction := Input.get_axis("block_and_move_backwards_singleplayer", "move_forward_singleplayer")
+		
+		#Makes blocking true if the player is moving backwards
+		isBlocking = Input.is_action_pressed("block_and_move_backwards_singleplayer") && direction < 0
 
 		# Detect double-tap for dodge
 		var current_time := Time.get_ticks_msec() / 1000.0
@@ -59,9 +71,9 @@ func _physics_process(delta: float) -> void:
 			last_tap_time_left = current_time
 
 		# Normal movement if not dodging
-		if direction > 0:
+		if direction > 0 && isAttacking == false:
 			velocity.x = direction * SPEED
-		elif direction < 0: #move backwards, currently moving backwards makes you move slower, but also would likely need to add blocking feature here
+		elif direction < 0 && isAttacking == false: #move backwards, currently moving backwards makes you move slower
 			velocity.x = (direction * SPEED) / 2
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
